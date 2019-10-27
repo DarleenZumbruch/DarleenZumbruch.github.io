2a01abecdfbffcf0ef3796a3ebff8ff6f91e8ab0
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-10-27 02:56:14 +0200

    Replace image with text

--- javascript.js
+++ javascript.js
@@ -1,14 +1,36 @@
 var img;
 
-
 function preload() {
     img = loadImage('images/stefan-sagmeister.jpg');
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    textFont('Barlow');
+    textSize (28);
 }
 
 function draw() {
-    image(img, 0, 0, window.innerWidth, window.innerHeight)
+    var x = 0;
+    var y = 0;
+
+    while (y < height) {
+
+        push();
+        translate(x, y);
+
+        //text position
+        text("Stefan Sagmeister ", 0, 0);
+        x += textWidth("Stefan Sagmeister ");
+
+        pop();
+
+        //linebreak
+        if (x + textWidth("Stefan Sagmeister ") >= width) {
+            x = 0;
+            y += 30;
+        }
+    }
+
+    noLoop();
 }
