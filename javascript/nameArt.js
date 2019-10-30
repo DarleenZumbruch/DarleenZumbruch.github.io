55a2444a0226fddada2d299931a46be7a101d51d
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-10-30 22:22:59 +0100

    Add Font as Path and create points/dots as outline

--- javascript/nameArt.js
+++ javascript/nameArt.js
@@ -1,21 +1,27 @@
 var profileName = 'Stefan Sagmeister';
 var textWelcome = 'Welcome \n' + profileName;
 
+var typeface;
+var textToPoints;
+
 function preload(){
-    barlow = loadFont('../font/Barlow-SemiBold.otf');
+    typeface = loadFont('font/Barlow-SemiBold.otf');
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    textSize(80);
-    textAlign(CENTER, CENTER);
+
+    textToPoints = typeface.textToPoints(textWelcome, window.innerWidth/5, window.innerHeight/2, 80,{
+        sampleFactor: 0.25,
+        simplifyThreshold: 0
+    });
 }
 
 function draw() {
-    text(textWelcome, 0, 0, window.innerWidth, window.innerHeight - 200);
-    if (mouseIsPressed) {
-        noFill();
-        stroke(0, 255, 0);
-        rect(mouseX, mouseY, 200, 200);
+
+    //points as outline
+    for(var i =0; i< textToPoints.length; i++){
+        fill(0);
+        ellipse(textToPoints[i].x, textToPoints[i].y, 2,2);
     }
 }
