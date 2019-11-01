cf481e2819073525587ea7722cc793ceaa5e1483
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-01 17:21:40 +0100

    generate random letter

--- javascript/nameArt.js
+++ javascript/nameArt.js
@@ -10,7 +10,8 @@ function preload(){
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
+    textSize(100)
+;
     document.getElementById("TextWelcomePlaceholder").innerHTML = textWelcome;
     x = width/2 - TextWelcomePlaceholder.clientWidth/2;
     y = height/2 - TextWelcomePlaceholder.clientHeight/2;
@@ -18,6 +19,9 @@ function setup() {
     textToPoints = typeface.textToPoints(textWelcome, x, y, 80,{
         sampleFactor: 0.25
     });
+
+    letter = new Letter(windowWidth/2, windowWidth/2);
+    letter.RandomLetter();
 }
 
 function draw() {
@@ -27,4 +31,23 @@ function draw() {
         fill(0);
         ellipse(textToPoints[i].x, textToPoints[i].y, 1,1);
     }
+
+    letter.render();
+}
+
+function Letter(x,y) {
+    this.x = x;
+    this.y = y;
+    this.value;
+
+    this.RandomLetter = function() {
+        this.value = String.fromCharCode(
+            0x00 + round(random(65,90))
+        );
+    };
+
+    this.render = function () {
+        fill(0);
+        text(this.value, this.x, this.y);
+    };
 }
