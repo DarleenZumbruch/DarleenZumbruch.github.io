a0d3e9002660e3da0580dcfe7bbd53ab33c68174
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-24 09:47:15 +0100

    Output multiple images

--- javascript/images.js
+++ javascript/images.js
@@ -1,13 +1,18 @@
-var img;
+var img = [];
 
 function preload(){
-    img = loadImage('../images/stefan-sagmeister.jpg');
+    for (var i = 0; i <15; i++) {
+        img[i] = loadImage("../images/post_" + i + ".jpg");
+    }
 }
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+
 }
 
 function draw() {
-    image(img, 0, 0, img.width / 5, img.height / 5)
+    for (var i = 0; i < 15; i++) {
+        image(img[i], random(window.innerWidth-500), random(window.innerHeight-500), 500, 500);
+    }
 }
