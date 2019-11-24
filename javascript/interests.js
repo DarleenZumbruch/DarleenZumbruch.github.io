cdd128cc5d63296633b587d1cdf6dd9483fe0fae
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-24 16:38:50 +0100

    add ellipses

--- javascript/interests.js
+++ javascript/interests.js
@@ -1,6 +1,10 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(0);
+    noStroke();
 }
 
 function draw() {
+    fill(255);
+    ellipse(random(windowWidth), random(windowHeight), 30, 30);
 }
