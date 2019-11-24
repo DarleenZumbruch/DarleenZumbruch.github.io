ffa7f735847aade52dffa3df7b5306dc3f1647b2
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-24 18:21:27 +0100

    Add Favorite Things Array

--- javascript/interests.js
+++ javascript/interests.js
@@ -1,10 +1,19 @@
+var music = ["Rolling Stones","The Talking Heads","Lou Reed", "Iggy Pop", "Pixies", "Nick Cave"];
+var books = ["Consider the Lobster","The Corrections","Edward de Bono’s Thinking Course", "The Last Place on Earth", "On Beauty"];
+var designers = ["Tibor Kalman", "James Victore"];
+var words = ["Design", "Happy", "Logo", "Passion", "Brilliant", "Love"];
+
+var wordIndex = 0;
+
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
-    noStroke();
 }
 
 function draw() {
     fill(255);
-    ellipse(random(windowWidth), random(windowHeight), 30, 30);
+    text(music[wordIndex], random(windowWidth), random(windowHeight))
+
+    wordIndex ++;
 }
