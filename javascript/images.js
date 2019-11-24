42c4fbfecac884b6226159d15816a066ac9c0bb1
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-24 09:49:05 +0100

    position picture over window-size

--- javascript/images.js
+++ javascript/images.js
@@ -8,11 +8,12 @@ function preload(){
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    imageMode(CENTER);
 
 }
 
 function draw() {
     for (var i = 0; i < 15; i++) {
-        image(img[i], random(window.innerWidth-500), random(window.innerHeight-500), 500, 500);
+        image(img[i], random(window.innerWidth), random(window.innerHeight), 500, 500);
     }
 }
