d599e3f1ea3282ba7994191733aea63610c75b94
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-26 18:39:19 +0100

    add file link

--- javascript/relations.js
+++ javascript/relations.js
@@ -1,5 +1,11 @@
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
 }
 
 function draw() {
+    background(0,10);
+    fill(255);
+    stroke(255);
+    strokeWeight(2);
 }
