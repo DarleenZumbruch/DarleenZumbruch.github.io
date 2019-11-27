262abf10d929ba886356bd675a23afe0105ae51a
darleenzumbruch <darleen.zumbruch@code.berlin>
2019-11-27 21:36:18 +0100

    add particle System

--- javascript/relations.js
+++ javascript/relations.js
@@ -1,11 +1,63 @@
-function setup() {
+var particle;
+
+function setup (){
     createCanvas(windowWidth, windowHeight);
-    background(255);
+    background(0);
+
+    privacyColors = [color('rgb(242, 61, 61)'), color('rgb(75,179,104)'), color('rgb(242, 61, 61)')];
+    categoryColors = [color('rgb(255,255,255)'), color('rgb(170,170,170)')];
+
+    particleSystem();
 }
 
-function draw() {
-    background(0,10);
+function particleSystem(){
+    particle = [];
+
+    for(var i = 0; i < 100; i++) {
+        var location = createVector(random(50,windowWidth-50), random(50, windowHeight-250));
+        var radius = random(5, 15);
+
+        particle.push({
+            location: location,
+            radius: radius,
+            Pcol: privacyColors[floor(random(privacyColors.length))],
+            Ccol: categoryColors[floor(random(categoryColors.length))]
+        });
+    }
+}
+
+function draw (){
+    background(0,);
     fill(255);
     stroke(255);
     strokeWeight(2);
+
+    //Connectors
+    stroke(255);
+    for (var i = 0; i < 100; i++){
+        for (var j = 0; j < 100; j++){
+            if (pow (particle[i].location.x - particle[j].location.x,2) + pow(particle[i].location.y-particle[j].location.y,2) < pow(200,2) ){
+                fill(255);
+                stroke(50) ;
+                line(particle[i].location.x,particle[i].location.y,particle[j].location.x,particle[j].location.y);
+            }
+        }
+    }
+
+    //Show Particle
+    for(var i = 0; i < 100; i++){
+        fill(particle[i].Ccol);
+        stroke(particle[i].Pcol);
+        strokeWeight(3);
+        ellipse(particle[i].location.x , particle[i].location.y, particle[i].radius*5, particle[i].radius*5);
+
+        particle[i].location.x = (particle[i].location.x + width ) % width;
+        particle[i].location.y = (particle[i].location.y + height )% height;
+    }
 }
+
+function mousePressed(){
+    particleSystem();
+}
+
+  
